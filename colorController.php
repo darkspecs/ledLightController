621e69fad8a5bfbab9807b3689226710e74bf4ec
Brandon Kern <brandonkern.001@gmail.com>
2020-07-25 22:58:27 -0400

    commit

--- colorController.php
+++ colorController.php
@@ -1,12 +1,10 @@
 <?php
 function convertHexToRGB($hex)
 {
-    if (strlen($hex) != 7) {
+    if (strlen($hex) != 7 || $hex[0] != "#") {
         throw new Exception('Invalid hex format.');
     }
-    if ($hex[0] != "#") {
-        throw new Exception('Invalid hex format.');
-    }
+
     $out = "(" . hexdec($hex[1] . $hex[2]) . ",";
     $out = $out . hexdec($hex[3] . $hex[4]) . ",";
     $out = $out . hexdec($hex[5] . $hex[6]) . ")";
@@ -14,16 +12,25 @@ function convertHexToRGB($hex)
 }
 convertHexToRGB("#ff0000");
 
-if(isset($_POST['color'])){
-    echo("works");
+if (isset($_POST['color'])) {
+    $hex = convertHexToRGB($_POST['color']);
+    echo ($hex);
 }
-
 ?>
 <html>
-
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 <body>
-    <input type="color" id="color" name="color" value="#ff0000"><br><br>
-    <input type="submit">
+    <form action="" method="post">
+        <input type="color" id="color" name="color" value="<?php 
+            if(isset($_POST['color'])){
+                echo($_POST['color']);
+            }else{
+                echo("#ffffff");
+            }
+        ?>">
+        <br><br>
+        <input type="submit">
+    </form>
 </body>
 
 </html>
