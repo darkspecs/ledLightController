3939af6acd83cde40e05fe8f15ea65902ef1bf95
Brandon Kern <brandonkern.001@gmail.com>
2020-07-26 01:49:19 -0400

    commit

--- colorPicker.php
+++ colorPicker.php
@@ -16,7 +16,11 @@ if (isset($_POST['color'])) {
             throw new Exception('Invalid hex format.');
         }
         $colormode = 0;
-        $color[0] = $colormode;
+        if (isset($_POST['switch'])) {
+            $color[0] = 1;
+        } else {
+            $color[0] = 0;
+        }
         $color[1] = $_POST['color'];
         writeJsonFile($color);
     } catch (Exception $e) {
@@ -28,12 +32,18 @@ $data = json_decode($jsondata);
 ?>
 <html>
 <meta name="viewport" content="width=device-width, initial-scale=2.0, maximum-scale=2.0">
+
 <body>
     <form action="" method="post">
-        <input type="color" id="color" name="color" value="<?php echo($data[1]);?>">
+        <input type="color" id="color" name="color" value="<?php echo ($data[1]); ?>">
+        Enabled:
+        <label class="switch">
+            <input type="checkbox" name="switch" <?php if ($data[0] == 1) {
+                                                        print("checked");
+                                                    } ?>>
+        </label>
         <br><br>
         <input type="submit">
     </form>
 </body>
-
 </html>
--- controller.py
+++ controller.py
@@ -1,11 +1,40 @@
 import json
-import RPi.GPIO as GPIO
+import os
+import time
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+
+#import RPi.GPIO as GPIO
 
 def convertHexToRGB(hex):
     h = hex.lstrip('#')
     return tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
 
-with open('data.json') as f:
-  data = json.load(f)
+def readData():
+    with open('data.json') as f:
+        return json.load(f)
+def printData():
+    data = readData()
+    print("Enabled: {}, RGB Values: {}".format(data[0],data[1]))
 
-print(convertHexToRGB(data[1]))
+class MyEventHandler(PatternMatchingEventHandler):
+    def on_modified(self, event):
+        super(MyEventHandler, self).on_modified(event)
+        print("Changed.")
+        # FIXME python recognizes change, needs to take change and push to led lights
+        printData()
+
+event_handler = MyEventHandler(patterns='dat*.json')
+
+my_observer = Observer()
+my_observer.schedule(event_handler, ".", recursive=True)
+
+printData()
+
+my_observer.start()
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    my_observer.stop()
+my_observer.join()
