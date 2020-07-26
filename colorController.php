0f2749dc48a0ab19d521a8adbaa5a14cb14aedcd
Brandon Kern <brandonkern.001@gmail.com>
2020-07-25 22:34:14 -0400

    commit

--- colorController.php
+++ colorController.php
@@ -1,16 +1,25 @@
 <?php
-    function convertHexToRGB($hex){
-        if(strlen($hex) != 7){
-            throw new Exception('Invalid hex format.');
-        }
-        if($hex[0] != "#"){
-            throw new Exception('Invalid hex format.');
-        }
-        $out = "(" . hexdec($hex[1] . $hex[2]) . ",";
-        $out = $out . hexdec($hex[3] . $hex[4]) . ",";
-        $out = $out . hexdec($hex[5] . $hex[6]) . ")";
-        return $out;
+function convertHexToRGB($hex)
+{
+    if (strlen($hex) != 7) {
+        throw new Exception('Invalid hex format.');
     }
-    convertHexToRGB("#ff0000");
+    if ($hex[0] != "#") {
+        throw new Exception('Invalid hex format.');
+    }
+    $out = "(" . hexdec($hex[1] . $hex[2]) . ",";
+    $out = $out . hexdec($hex[3] . $hex[4]) . ",";
+    $out = $out . hexdec($hex[5] . $hex[6]) . ")";
+    return $out;
+}
+convertHexToRGB("#ff0000");
 
 ?>
+<html>
+
+<body>
+    <input type="color" id="favcolor" name="favcolor" value="#ff0000"><br><br>
+    <input type="submit">
+</body>
+
+</html>
