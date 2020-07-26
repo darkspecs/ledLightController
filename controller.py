bda6dadf7e0e8672bf9604c65ac81a901313a9ba
Brandon Kern <brandonkern.001@gmail.com>
2020-07-26 02:31:38 -0400

    commit

--- controller.py
+++ controller.py
@@ -1,6 +1,8 @@
 import json
 import os
 import time
+import threading
+
 from watchdog.observers.polling import PollingObserver as Observer
 from watchdog.events import PatternMatchingEventHandler
 
@@ -17,20 +19,22 @@ def readData():
 def printData():
     data = readData()
     print("Enabled: {}, RGB Values: {}".format(data[0],convertHexToRGB(data[1])))
+def watchJsonFile():
+    class MyEventHandler(PatternMatchingEventHandler):
+        def on_modified(self, event):
+            super(MyEventHandler, self).on_modified(event)
+            # FIXME python recognizes change, needs to take change and push to led lights
+            printData()
 
-class MyEventHandler(PatternMatchingEventHandler):
-    def on_modified(self, event):
-        super(MyEventHandler, self).on_modified(event)
-        # FIXME python recognizes change, needs to take change and push to led lights
-        printData()
-
-event_handler = MyEventHandler(patterns='dat*.json')
-my_observer = Observer()
-my_observer.schedule(event_handler, ".", recursive=True)
-my_observer.start()
-try:
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    my_observer.stop()
-my_observer.join()
+    event_handler = MyEventHandler(patterns='dat*.json')
+    my_observer = Observer()
+    my_observer.schedule(event_handler, ".", recursive=True)
+    my_observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        my_observer.stop()
+    my_observer.join()
+x = threading.Thread(target=watchJsonFile)
+x.start()
