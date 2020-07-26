d58bbc944b4d58b44951743c579bc6a214b23aee
Brandon Kern <brandonkern.001@gmail.com>
2020-07-25 23:10:01 -0400

    commit

--- colorController.php
+++ colorController.php
@@ -4,30 +4,30 @@ function convertHexToRGB($hex)
     if (strlen($hex) != 7 || $hex[0] != "#") {
         throw new Exception('Invalid hex format.');
     }
-
-    $out = "(" . hexdec($hex[1] . $hex[2]) . ",";
-    $out = $out . hexdec($hex[3] . $hex[4]) . ",";
-    $out = $out . hexdec($hex[5] . $hex[6]) . ")";
+    $out[0] = hexdec($hex[1] . $hex[2]); //red
+    $out[1] = hexdec($hex[3] . $hex[4]); //green
+    $out[2] = hexdec($hex[5] . $hex[6]); //blue
     return $out;
 }
 convertHexToRGB("#ff0000");
 
 if (isset($_POST['color'])) {
     $hex = convertHexToRGB($_POST['color']);
-    echo ($hex);
+    echo (var_dump($hex));
 }
 ?>
 <html>
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=2.0, maximum-scale=2.0">
+
 <body>
     <form action="" method="post">
-        <input type="color" id="color" name="color" value="<?php 
-            if(isset($_POST['color'])){
-                echo($_POST['color']);
-            }else{
-                echo("#ffffff");
-            }
-        ?>">
+        <input type="color" id="color" name="color" value="<?php
+                                                            if (isset($_POST['color'])) {
+                                                                echo ($_POST['color']);
+                                                            } else {
+                                                                echo ("#ffffff");
+                                                            }
+                                                            ?>">
         <br><br>
         <input type="submit">
     </form>
