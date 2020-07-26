4d70067c1fbc72180369aa7b406b88885d10b284
Brandon Kern <brandonkern.001@gmail.com>
2020-07-25 22:23:30 -0400

    master

--- colorController.php
+++ colorController.php
@@ -1,14 +1,16 @@
 <?php
     function convertHexToRGB($hex){
         if(strlen($hex) != 7){
-            return "Error";
+            throw new Exception('Invalid hex format.');
+        }
+        if($hex[0] != "#"){
+            throw new Exception('Invalid hex format.');
         }
         $out = "(" . hexdec($hex[1] . $hex[2]) . ",";
         $out = $out . hexdec($hex[3] . $hex[4]) . ",";
         $out = $out . hexdec($hex[5] . $hex[6]) . ")";
         return $out;
     }
-
-    
+    convertHexToRGB("#ff0000");
 
 ?>
