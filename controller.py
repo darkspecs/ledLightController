eaf1dacf3179e4cddf70a1a63547619e75ecd6e0
darkspecs <68797242+darkspecs@users.noreply.github.com>
2020-07-27 20:54:49 -0400

    Update controller.py

--- controller.py
+++ controller.py
@@ -6,7 +6,13 @@ import threading
 from watchdog.observers.polling import PollingObserver as Observer
 from watchdog.events import PatternMatchingEventHandler
 
-#import RPi.GPIO as GPIO
+import pigpio
+
+RED = 2
+GREEN = 3
+BLUE = 4
+
+pi = pigpio.pi()
 
 def convertHexToRGB(hex):
     if len(hex) != 7 or hex[0] != "#":
@@ -19,11 +25,25 @@ def readData():
 def printData():
     data = readData()
     print("Enabled: {}, RGB Values: {}".format(data[0],convertHexToRGB(data[1])))
+def updateColors():
+    data = readData()
+    if data[0] == 1:
+        data = convertHexToRGB(data[1])
+        pi.set_PWM_dutycycle(RED, data[0])
+        pi.set_PWM_dutycycle(GREEN, data[1])
+        pi.set_PWM_dutycycle(BLUE, data[2])
+    else: 
+        pi.set_PWM_dutycycle(RED, 0)
+        pi.set_PWM_dutycycle(GREEN, 0)
+        pi.set_PWM_dutycycle(BLUE, 0)
+updateColors()
+printData()
 def watchJsonFile():
     class MyEventHandler(PatternMatchingEventHandler):
         def on_modified(self, event):
             super(MyEventHandler, self).on_modified(event)
             # FIXME python recognizes change, needs to take change and push to led lights
+            updateColors()
             printData()
 
     event_handler = MyEventHandler(patterns='dat*.json')
